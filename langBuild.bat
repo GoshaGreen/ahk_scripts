468cbd0633f50bf0ec65c724d9d9872412f06754
GoshaGreen <goshadub@gmail.com>
2022-04-12 11:30:39 +0300

    Fix appdata variable in build script

--- langBuild.bat
+++ langBuild.bat
@@ -1,10 +1,10 @@
 REM @echo off
 
 set DIR=%~dp0
-del "%DIR%lang.exe" "%APP_DATA%\Microsoft\Windows\Start Menu\Programs\Startup\lang.lnk"
+del "%DIR%lang.exe" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\lang.lnk"
 
 "%PROGRAMFILES%\AutoHotkey\Compiler\Ahk2Exe.exe" /in "%DIR%lang.ahk" /out "%DIR%lang.exe"
 
-mklink "%APP_DATA%\Microsoft\Windows\Start Menu\Programs\Startup\lang.lnk" "%DIR%lang.exe"
+mklink "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\lang.lnk" "%DIR%lang.exe"
 
 "%DIR%lang.exe"
