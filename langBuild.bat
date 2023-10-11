770a5cc824a77f79f99e908b48d15d43943ba6d2
GoshaGreeN <goshadub@gmail.com>
2023-10-11 11:31:16 +0200

    Add additional path to AHK

--- langBuild.bat
+++ langBuild.bat
@@ -5,7 +5,9 @@ del "%DIR%lang.exe" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\lan
 
 @REM "%PROGRAMFILES%\AutoHotkey\Compiler\Ahk2Exe.exe" /in "%DIR%lang2.ahk" /out "%DIR%lang.exe"
 
-"%AppData%\..\Local\Programs\AutoHotkey\Compiler\Ahk2Exe.exe" /in "%DIR%lang2.ahk" /out "%DIR%lang.exe"
+set COMPILLER_PATH="%AppData%\..\Local\Programs\AutoHotkey\Compiler\"
+set COMPILLER_PATH="C:\Program Files\AutoHotkey\Compiler"
+%COMPILLER_PATH%"\Ahk2Exe.exe" /in "%DIR%lang2.ahk" /out "%DIR%lang.exe"
 
 mklink "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\lang.lnk" "%DIR%lang.exe"
 
